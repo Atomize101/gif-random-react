b066ed1e599c69956ab3baff85b7caf487503d46
Chris Patrick <atomize101@gmail.com>
2020-11-17 13:00:22 -0500

    Refactor to run fetchGif from handleClick

--- src/components/Random_V1.jsx
+++ src/components/Random_V1.jsx
@@ -4,20 +4,28 @@ import axios from 'axios';
 const Random = () => {
 	const [gif, setGif] = useState('');
 	// Component did mount ... first render
-	useEffect(() => {
-		const fetchGif = async () => {
-			const url = `https://api.giphy.com/v1/gifs/random?api_key=rwtP5pLyt6ZSg0BdC4Gjmv105LH6lm9U`;
-			const { data } = await axios.get(url); // Promise
 
-			const imageSrc = data.data.images.downsized_large.url;
-			setGif(imageSrc);
-		};
+	const fetchGif = async () => {
+		const url = `https://api.giphy.com/v1/gifs/random?api_key=rwtP5pLyt6ZSg0BdC4Gjmv105LH6lm9U`;
+		const { data } = await axios.get(url); // Promise
+
+		const imageSrc = data.data.images.downsized_large.url;
+		setGif(imageSrc);
+	};
+
+	useEffect(() => {
 		fetchGif();
 	}, []);
+
+	const handleClick = () => {
+		fetchGif();
+	};
+
 	return (
 		<div>
 			<h1>Random Gif</h1>
 			<img width="500" src={gif} alt="Random Gif" />
+			<button onClick={handleClick}>Click for new!</button>
 		</div>
 	);
 };
