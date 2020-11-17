a245124e0d1883d5fa76596b5128ab00dee701f6
Chris Patrick <atomize101@gmail.com>
2020-11-17 12:38:32 -0500

    Fetching data

--- src/components/Random_V1.jsx
+++ src/components/Random_V1.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Random = () => {
+	// Component did mount ... first render
+	useEffect(() => {
+		const fetchGif = async () => {
+			const url = `https://api.giphy.com/v1/gifs/random?api_key=rwtP5pLyt6ZSg0BdC4Gjmv105LH6lm9U`;
+			const { data } = await axios.get(url); // Promise
+
+			console.log(data);
+		};
+		fetchGif();
+	}, []);
 	return (
 		<>
 			<h1>Random</h1>
