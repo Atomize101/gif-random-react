ef607a1639b9b9f47ad196a455d97db6bef92ca2
Chris Patrick <atomize101@gmail.com>
2020-11-17 12:57:30 -0500

    Random gif now displaying. Setting state

--- src/components/Random_V1.jsx
+++ src/components/Random_V1.jsx
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-
 const Random = () => {
+	const [gif, setGif] = useState('');
 	// Component did mount ... first render
 	useEffect(() => {
 		const fetchGif = async () => {
 			const url = `https://api.giphy.com/v1/gifs/random?api_key=rwtP5pLyt6ZSg0BdC4Gjmv105LH6lm9U`;
 			const { data } = await axios.get(url); // Promise
 
-			console.log(data);
+			const imageSrc = data.data.images.downsized_large.url;
+			setGif(imageSrc);
 		};
 		fetchGif();
 	}, []);
 	return (
-		<>
-			<h1>Random</h1>
-		</>
+		<div>
+			<h1>Random Gif</h1>
+			<img width="500" src={gif} alt="Random Gif" />
+		</div>
 	);
 };
 
